6be0d64922f747c5c847648e8ecaf015c35084ff
patricktrunfio <trunfio@usc.edu>
2021-10-25 16:35:20 -0700

    update

--- McDermott/10_22 Data/Pintle_DataRead_10_25.m
+++ McDermott/10_22 Data/Pintle_DataRead_10_25.m
@@ -22,15 +22,15 @@ pint_out_d = 0.742/39.37; % m
 filenamePrefix = "Data\Pintle\Pintle_10-22_";
 filenameExtension = ".csv";
 presVals = ["50","100", "125" ,"150"];
-nRuns = [3,1,3,1];
+nRuns = [3,1,1,1];
 data = struct();
-start = [8800, 9200, 11584, 14240];
-stop = [14500, 14000, 17941, 20679];
+start = {[9200, 10316, 12416], [9252], [10390], [10006] };
+stop = {[15832, 15711, 14818], [15440], [18208], [13781]};
 for i = 1:length(presVals)
     for n = 1:nRuns(i)  
-    filename = filenamePrefix + presVals(i) +"-"+ string(nRuns(i)) + filenameExtension;
+    filename = filenamePrefix + presVals(i) +"-"+ string(n) + filenameExtension;
     tmpData=csvread(filename,1,0);
-    fieldName = "psi" + presVals(i) + "_" + string(nRuns(i));
+    fieldName = "psi" + presVals(i) + "_" + string(n);
     data.(fieldName).t = tmpData(:,1); % Time in seconds
     data.(fieldName).p = tmpData(:,7); % Pressure upstream of article in psi
     data.(fieldName).lc_1 = tmpData(:,16); % Load Cell (lbf)
@@ -41,35 +41,35 @@ for i = 1:length(presVals)
     clear tmpData;
     end
 end
-
+ %% Data Analysis
  for i = 1:length(presVals)
-%Clean up Data
-    fieldName = "psi" + presVals(i) + "_" + string(nRuns(i));
-    data.(fieldName).t = data.(fieldName).t(start(i):stop(i));
-    data.(fieldName).p = data.(fieldName).p(start(i):stop(i));
-    data.(fieldName).m = data.(fieldName).m(start(i):stop(i));
-%Calculate values
-    data.(fieldName).p_avg = mean(data.(fieldName).p)*6894.75; % Pa.
-    data.(fieldName).mFlowRate = (data.(fieldName).m(end)-data.(fieldName).m(1))/(data.(fieldName).t(end)-data.(fieldName).t(1))*(1/2.205); % kg/s.
-    data.(fieldName).Cd = data.(fieldName).mFlowRate/(A*sqrt(2*rho*data.(fieldName).p_avg));
-%Plots
-    figure
-    plot(data.(fieldName).t,data.(fieldName).p)
-    hold on
-    plot(data.(fieldName).t,data.(fieldName).m)
-    title(presVals(i))
-    
-    figure
-    hold on
-    plot(data.(fieldName).t, data.(fieldName).lc_1(start(i):stop(i)))
-    plot(data.(fieldName).t, data.(fieldName).lc_2(start(i):stop(i)))
-    plot(data.(fieldName).t, data.(fieldName).lc_3(start(i):stop(i)))
-    plot(data.(fieldName).t, data.(fieldName).lc_4(start(i):stop(i)))
-    legend('lc_1','lc_2','lc_3','lc_4')
-    title(presVals(i))
+     for n = 1:nRuns(i)
+        %Clean up Data
+        filename = filenamePrefix + presVals(i) +"-"+ string(n) + filenameExtension;
+        fieldName = "psi" + presVals(i) + "_" + string(n);
+        data.(fieldName).t = data.(fieldName).t(start{i}(n):stop{i}(n));
+        data.(fieldName).p = data.(fieldName).p(start{i}(n):stop{i}(n));
+        data.(fieldName).m = data.(fieldName).m(start{i}(n):stop{i}(n));
+        %Calculate values
+        data.(fieldName).p_avg = mean(data.(fieldName).p)*6894.75; % Pa.
+        data.(fieldName).mFlowRate = (data.(fieldName).m(end)-data.(fieldName).m(1))/(data.(fieldName).t(end)-data.(fieldName).t(1))*(1/2.205); % kg/s.
+        data.(fieldName).Cd = data.(fieldName).mFlowRate/(A*sqrt(2*rho*data.(fieldName).p_avg));
+        %Plots
+        figure
+        plot(data.(fieldName).t,data.(fieldName).p)
+        hold on
+        plot(data.(fieldName).t,data.(fieldName).m)
+        title(fieldName)
+        
+        figure
+        hold on
+        plot(data.(fieldName).t, data.(fieldName).lc_1(start{i}(n):stop{i}(n)))
+        plot(data.(fieldName).t, data.(fieldName).lc_2(start{i}(n):stop{i}(n)))
+        plot(data.(fieldName).t, data.(fieldName).lc_3(start{i}(n):stop{i}(n)))
+        plot(data.(fieldName).t, data.(fieldName).lc_4(start{i}(n):stop{i}(n)))
+        legend('lc_1','lc_2','lc_3','lc_4')
+        title(fieldName)
+     end
  end
-
-% Finding Pressure Loss after PT on annulus 
-pipeD = .5; % [in]
 
 
