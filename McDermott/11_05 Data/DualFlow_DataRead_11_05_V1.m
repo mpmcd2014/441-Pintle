361237910b4fde62c72c717e6089f90d2db5a930
mpmcd2014 <55304610+mpmcd2014@users.noreply.github.com>
2021-11-07 18:20:27 -0800

    DualFlow DataRead 11/05

---
+++ McDermott/11_05 Data/DualFlow_DataRead_11_05_V1.m
@@ -0,0 +1,162 @@
+%{
+10/24/2021
+Initial Testing Analysis
+%}
+clc; clear; close all;
+%% CONSTANTS
+g   = 9.80665;  % m/s^2.
+rho = 998; % kg/m^3. Density of water at 70 deg F.
+% Pintle Geometry
+CdP     = 0.60;
+orif_d = 0.0625/39.37; % m. Orifice Diameter [in]
+n_orif = 20;
+AP = pi*(orif_d/2)^2*n_orif; % m^2. Pintle Flow area [in^2]
+
+% Annulus Geometry
+CdAnn   = 0.57;
+ann_d = .766/39.37; % m
+pint_out_d = 0.7415/39.37; % m
+AAnn = pi*(ann_d/2)^2 - pi*(pint_out_d/2)^2; % m^2
+
+% ============== 1 CORRESPONDS TO RADIAL/PINTLE ===============
+% ============== 2 CORRESPONDS TO AXIAL/ANNULUS ===============
+
+Cd  = [CdP,CdAnn];
+A   = [AP,AAnn];
+BF  = n_orif*orif_d/(pi*pint_out_d);
+
+thetaPred = @(T) 0.6243*atan(3.377*T);
+
+printResults = {'TMRAvg','ThetaAvg','mTotCalc','mTotMeas'};
+%% Read in Data
+%%some intializations
+
+filenamePrefix = "Data\DualFlow";
+filenameExtension = ".csv";
+presVals = [205,47];
+nRuns = [4];
+data = struct();
+start = [8800, 9200, 11584, 14240];
+stop = [14500, 14000, 17941, 20679];
+expandRange = 2000;
+
+freqThreshold = 0.1;        % Include frequencies that contribute more than 10% of amplitude.
+Pvary       = 0.8;          % Fraction of maximum pressure through which pressure can vary during test.
+nTMR      = size(presVals,1);
+
+testNames = {};
+for n1 = 1:size(presVals,1)
+    for n2 = 1:nRuns(n1)
+        filename = filenamePrefix +"A"+string(presVals(n1,1))+"P"+string(presVals(n1,2))+"-"+string(n2)+filenameExtension;
+        tmpData=csvread(filename,1,0);
+        fieldName = "A" + string(presVals(n1,1))+"P"+string(presVals(n1,2))+"R"+string(n2);
+        data.(fieldName).t = tmpData(:,1);                  % s. Time
+        data.(fieldName).res    = repmat(struct('P',[],'Psmooth',[],'minInd',[],'maxInd',[]),2,1);
+        data.(fieldName).res(2).P = tmpData(:,6)*6894.75;           % Pa. Pressure upstream of annulus.
+        data.(fieldName).res(1).P = tmpData(:,7)*6894.75;          % Pa. Pressure upstream of pintle.
+        %data.(fieldName).ann    = struct();
+        %data.(fieldName).pin    = struct();
+        %data.(fieldName).ann.P  = tmpData(:,6)*6894.75;          % Pa. Pressure upstream of annulus.
+        %data.(fieldName).pin.P  = tmpData(:,7)*6894.75;       % Pa. Pressure upstream of pintle.
+        if sum(data.(fieldName).res(1).P) == 0 || sum(data.(fieldName).res(2).P) == 0
+            warning(fieldName+" contains no pressure data.")
+        else
+            testNames(end+1) = {fieldName};
+        end
+        %data.(fieldName).lc_1 = tmpData(:,16)*(1/2.205);    % kg. Load Cell
+        %data.(fieldName).lc_2 = tmpData(:,17)*(1/2.205);    % kg. Load Cell
+        %data.(fieldName).lc_3 = tmpData(:,18)*(1/2.205);    % kg. Load Cell
+        %data.(fieldName).lc_4 = tmpData(:,19)*(1/2.205);    % kg. Load Cell
+        data.(fieldName).m = sum(tmpData(:,16:19),2)*(1/2.205); % kg. Mass collected by all four load cells. (data.(fieldName).lc_1 + data.(fieldName).lc_2 +data.(fieldName).lc_3 + data.(fieldName).lc_4);    % kg.
+        clear tmpData;
+    end
+end
+
+%% Process Data
+summary = zeros(nTMR,4);
+UPRel = zeros(nTMR,1);
+mDotRel = zeros(nTMR,1);
+CdRel = zeros(nTMR,1);
+for n1 = 1:length(testNames)
+%Clean up Data
+    fieldName = testNames{n1};
+    %{
+    tmp = FilterData_V1(data.(fieldName).t,[data.(fieldName).ann.P,data.(fieldName).pin.P,data.(fieldName).m],freqThreshold);
+    data.(fieldName).ann.Psmooth = tmp(:,1);
+    data.(fieldName).pin.Psmooth = tmp(:,2);
+    data.(fieldName).msmooth = tmp(:,3);
+    
+    data.(fieldName).ann.minInd = find((data.(fieldName).Psmooth-Pvary*max(data.(fieldName).Psmooth))>0,1,'first');
+    data.(fieldName).ann.maxInd = find((data.(fieldName).Psmooth-Pvary*max(data.(fieldName).Psmooth))>0,1,'last');
+    %}
+    for n2 = 1:2
+        tmp = FilterData_V1(data.(fieldName).t,[data.(fieldName).res(n2).P,data.(fieldName).m],freqThreshold);
+        data.(fieldName).res(n2).Psmooth = tmp(:,1);
+        data.(fieldName).msmooth = tmp(:,2);
+        data.(fieldName).res(n2).mDot = Cd(n2)*A(n2)*sqrt(2*rho*data.(fieldName).res(n2).P);
+        
+        data.(fieldName).res(n2).minInd = find((data.(fieldName).res(n2).Psmooth - Pvary*max(data.(fieldName).res(n2).Psmooth))>0,1,'first');
+        data.(fieldName).res(n2).maxInd = find((data.(fieldName).res(n2).Psmooth - Pvary*max(data.(fieldName).res(n2).Psmooth))>0,1,'last');
+                
+        data.(fieldName).res(n2).Pavg = mean(data.(fieldName).res(n2).Psmooth(data.(fieldName).res(n2).minInd:data.(fieldName).res(n2).maxInd));
+        data.(fieldName).res(n2).mDotAvg = mean(data.(fieldName).res(n2).mDot(data.(fieldName).res(n2).minInd:data.(fieldName).res(n2).maxInd));
+    end
+    
+    dualFlow    = intersect(data.(fieldName).res(1).minInd:data.(fieldName).res(1).maxInd,data.(fieldName).res(2).minInd:data.(fieldName).res(2).maxInd);
+    
+    data.(fieldName).TMR    = (((data.(fieldName).res(1).mDot(dualFlow)).^2)/(rho*A(1)))./(((data.(fieldName).res(2).mDot(dualFlow)).^2)/(rho*A(2)));
+    data.(fieldName).Theta  = thetaPred(data.(fieldName).TMR)*180/pi;
+    
+    data.(fieldName).TMRAvg     = mean(data.(fieldName).TMR);
+    data.(fieldName).ThetaAvg = mean(data.(fieldName).Theta);
+    
+    data.(fieldName).mTotCalc   = trapz(data.(fieldName).t,data.(fieldName).res(1).mDot) + trapz(data.(fieldName).t,data.(fieldName).res(2).mDot);
+    data.(fieldName).mTotMeas   = data.(fieldName).m(end) - data.(fieldName).m(1);
+    
+    strLen  = char("%-"+string(15*length(printResults))+"s\n");
+    fprintf(strLen,"RESULTS: "+fieldName);
+    %fprintf('%60s\n',"RESULTS: "+fieldName)
+    for n2 = 1:length(printResults)
+        fprintf('%15s',printResults{n2});
+    end
+    fprintf('\n');
+    for n2 = 1:length(printResults)
+        fprintf('%15.2f',data.(fieldName).(printResults{n2}));
+    end
+    fprintf('\n');
+    %{
+    %Calculate values
+    data.(fieldName).p_avg = mean(data.(fieldName).Psmooth(minInd:maxInd)); % Pa.
+    [data.(fieldName).flowLine,data.(fieldName).S]= polyfit(data.(fieldName).t(minInd:maxInd),data.(fieldName).msmooth(minInd:maxInd),1);
+    [~,data.(fieldName).delta] = polyval(data.(fieldName).flowLine,data.(fieldName).t(minInd:maxInd),data.(fieldName).S);
+    %data.(fieldName).mDot = [data.(fieldName).flowLine(1) (data.(fieldName).msmooth(maxInd)-data.(fieldName).msmooth(minInd))/(data.(fieldName).t(maxInd)-data.(fieldName).t(minInd))*(1/2.205)]; % kg/s.
+    data.(fieldName).mDot = data.(fieldName).flowLine(1);
+    data.(fieldName).Cd = data.(fieldName).mDot/(A*sqrt(2*rho*data.(fieldName).p_avg));
+
+    % UNCERTAINTY CALCULATION
+    data.(fieldName).D = struct();
+    data.(fieldName).D.P = (max(data.(fieldName).Psmooth(minInd:maxInd)) - min(data.(fieldName).Psmooth(minInd:maxInd)))/2;
+    s1 = ((data.(fieldName).msmooth(end)+data.(fieldName).delta(end)) - (data.(fieldName).msmooth(1)-data.(fieldName).delta(1)))/(data.(fieldName).t(maxInd)-data.(fieldName).t(minInd));
+    s2 = ((data.(fieldName).msmooth(end)-data.(fieldName).delta(end)) - (data.(fieldName).msmooth(1)+data.(fieldName).delta(1)))/(data.(fieldName).t(maxInd)-data.(fieldName).t(minInd));
+    data.(fieldName).D.mDot = abs(s2-s1)/2;
+    %data.(fieldName).D.mDot = abs(data.(fieldName).m(1) - (data.(fieldName).msmooth(maxInd)-data.(fieldName).msmooth(minInd))/(data.(fieldName).t(maxInd)-data.(fieldName).t(minInd)));
+    % Relative Uncertainties
+    data.(fieldName).D.PRel = data.(fieldName).D.P/data.(fieldName).p_avg;
+    data.(fieldName).D.mDotRel = data.(fieldName).D.mDot/data.(fieldName).mDot;
+    data.(fieldName).D.CdRel   = sqrt(data.(fieldName).D.mDotRel^2 + (data.(fieldName).D.PRel*0.5)^2);
+
+    summary(n1,:) = [data.(fieldName).p_avg/1000,data.(fieldName).mDot,data.(fieldName).Cd,data.(fieldName).D.CdRel];
+    UPRel(n1) = data.(fieldName).D.PRel;
+    mDotRel(n1) = data.(fieldName).D.mDotRel;
+    CdRel(n1) = data.(fieldName).D.CdRel;
+    %}
+    %{
+    figure
+    plot(data.(fieldName).t,data.(fieldName).msmooth)
+    hold on
+    plot(data.(fieldName).t(minInd:maxInd),polyval(data.(fieldName).flowLine,data.(fieldName).t(minInd:maxInd)))
+    data.(fieldName).ax = gca();
+    data.(fieldName).ax.FontName = 'Times New Roman';
+    data.(fieldName).ax.FontSize = 18;
+    %}
+end
